707a3aec4dceec9cf923f4cb1990c1f4bd525a13
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-02-23 19:50:20 -0500

    added two functions to user controller

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,1 +1,28 @@
 const { User, Thought, Reaction } = require("../models");
+
+const getAllUsers = () => {
+	User.find()
+		.then((users) => {
+			if (!users) {
+				res.status(404).json({ message: "No users found" });
+			} else {
+				res.status(200).json(users);
+			}
+		})
+		.catch((err) => res.status(500).json(err));
+};
+
+const getUserById = () => {
+	User.findOne({ _id: req.params.userId })
+		.select("-__v")
+		.populate("thoughts")
+		.populate("friends")
+		.then((user) => {
+			if (!user) {
+				res.status(404).json({ message: "No user with that ID" });
+			} else {
+				res.status(200).json(user);
+			}
+		})
+		.catch((err) => res.status(500).json(err));
+};
--- routes/api/users-routes.js
+++ routes/api/users-routes.js
@@ -4,6 +4,9 @@ const {} = require("../../controllers/users-controller");
 // /api/users
 router.route("/");
 
+// /api/users/:userId
+router.route("/:userId");
+
 // /api/users/:userId/friends/:friendId
 router.route("/:userId/friends/:friendId");
 
